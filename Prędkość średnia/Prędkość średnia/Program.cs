47fa78751eb5c5ee853097de9638a23666ad993a
MatheoMONSTER <81566430+MatheoMONSTER@users.noreply.github.com>
2021-04-08 20:47:11 +0200

    Add files via upload

---
+++ Prędkość średnia/Prędkość średnia/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Prędkość_średnia
+{
+    class averageSpeed
+    {
+        private int s;
+        private int t;
+
+        public averageSpeed(int a, int b)
+        {
+            s = a;
+            t = b;
+        }
+        public void calculateSpeed()
+        {
+            Console.WriteLine("Input route distance (in kilometers): ");
+            s = int.Parse(Console.ReadLine());
+            Console.WriteLine("Input time: ");
+            t = int.Parse(Console.ReadLine());
+            Console.WriteLine("Average speed is: {0} ", s / t);
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            averageSpeed speed1 = new averageSpeed(0,0);
+            speed1.calculateSpeed();
+            Console.ReadKey();
+        }
+    }
+}
